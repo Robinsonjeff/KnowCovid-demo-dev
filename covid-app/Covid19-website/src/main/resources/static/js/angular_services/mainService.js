f03f639a40ea8197649657a30675f52fbfcb00dc
Cole Robinson <33127273+Robinsonjeff@users.noreply.github.com>
2023-04-12 13:45:22 -0500

    Update mainService.js

--- covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
+++ covid-app/Covid19-website/src/main/resources/static/js/angular_services/mainService.js
@@ -2,7 +2,7 @@ app.service('mainService', function($http, $q, $location, $window) {
 
 	//local
     //var serverURL = "http://localhost:9300/Covid-19/";
-    var serverURL = "http://35.173.136.220:9090/Covid-19/";
+    var serverURL = "http://3.95.177.220 :9090/Covid-19/";
     
 	return {
 	   callPostRestAPI : function(url, data) {
